3c0650f03edd1aaa4153f54244db7cdfafa982a5
hotfloppy <hotfloppy.6866@gmail.com>
2020-09-25 13:03:11 +0800

    simplified the checking process using built in command. no need to install additional program.

--- checkidle.sh
+++ checkidle.sh
@@ -3,23 +3,10 @@
 PATHNAME=$(dirname $(readlink -f $0))
 source ${PATHNAME}/config
 
-if [[ $(which xprintidle > /dev/null) ]]; then
-    apt install xprintidle -qq
-fi
-
 users=($(who | awk '{ print $1 }' | grep -v root | sort | uniq ))
 
-#echo ${users[@]}
-
-pts="pts"
-
 for user in ${users[@]}; do
-    disp=$(w | grep ${user:0:8} | grep init | awk '{ print $2 }')
-    if test ${disp#*$pts} != $disp; then continue; fi # ignore pts display
-    export DISPLAY=$disp
-    idlems=$(sudo -u $user xprintidle)
-    idlemin=$((($idlems/1000)/60))
-    echo -e "${bold}$user:${nc} ${idlemin} mins"
+  ls -lt /dev/pts | fgrep $user | head -1 | awk '{ print $3" was last active on "$7" "$8" "$9 }'
 done
 
 exit 0
