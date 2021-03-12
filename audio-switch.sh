9f2873b4f8b382b1857df201b71371051fcbe350
Pencinta Kucing <hotfloppy.6866@gmail.com>
2021-03-12 09:16:55 +0800

    audio-switch.sh now can automatically detect which card numbers is the FANTECH or HDA speaker

--- audio-switch.sh
+++ audio-switch.sh
@@ -5,25 +5,31 @@
 confdir="$HOME/.config/hfbox"
 conffile="audio-switch-toggle"
 
+cards_count=$(pactl list cards | grep Card | wc -l)
+
+#
+# Configure the "FANTECH OCTANE 7.1" or "HDA Intel PCH" to your headpone name and speaker name.
+# Retrieve the name using this command:
+#   $ pactl list cards | grep -i alsa.card_name
+#
+headphone=$(pactl list cards | grep -B 6 -i 'alsa.card_name = "FANTECH OCTANE 7.1"' | grep Card | awk -F'#' '{ print $2 }')
+speaker=$(pactl list cards | grep -B 6 -i 'alsa.card_name = "HDA Intel PCH"' | grep Card | awk -F'#' '{ print $2 }')
+
+for ((card=0; card<${cards_count}; card++)); do
+  pactl set-card-profile $card off
+done
+
 enable_headphone() {
   echo "Switched to Headphone"
   notify-send -t 2000 "Switched to Headphone"
-  pactl set-card-profile 0 off # hdmi
-  pactl set-card-profile 2 output:analog-stereo+input:multichannel-input # headphone
-  pactl set-card-profile 3 off # speaker
-  pactl set-card-profile 1 off # loopback
-  pactl set-card-profile 4 off # loopback
+  pactl set-card-profile $headphone output:analog-stereo+input:multichannel-input
   echo "headphone" > $confdir/$conffile
 }
 
 enable_speaker() {
   echo "Switched to Speaker"
   notify-send -t 2000 "Switched to Speaker"
-  pactl set-card-profile 0 off # hdmi
-  pactl set-card-profile 2 off # headphone
-  pactl set-card-profile 4 output:analog-stereo # speaker
-  pactl set-card-profile 1 off # loopback
-  pactl set-card-profile 3 off # loopback
+  pactl set-card-profile $speaker output:analog-stereo
   echo "speaker" > $confdir/$conffile
 }
 
